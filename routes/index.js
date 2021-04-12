58ec65c2bc60062a748fcb88c8b333d509d28769
Caroline Lynch <carolineablynch@gmail.com>
2021-04-12 16:12:08 -0400

    building get post and delete requests from teh api

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,47 @@
 require("isomorphic-fetch");
 const router = require("express").Router();
 const { Book } = require("../models");
+
+router.get("/api/books", async (req, res) => {
+    try {
+      const books = await Book.find();
+  
+      res.json(books);
+    } catch (error) {
+      res.sendStatus(500);
+  
+      throw error;
+    }
+  });
+  
+  router.post("/api/books", async (req, res) => {
+    try {
+      const io = req.app.get("socketio");
+      const { title, authors, description, image, link } = req.body;
+      const book = new Book({ title, authors, description, image, link });
+  
+      await book.save();
+      io.emit("bookSaved", title);
+      res.json(book);
+    } catch (error) {
+      res.sendStatus(500);
+  
+      throw error;
+    }
+  });
+  
+  router.delete("/api/books/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const book = await Book.findByIdAndDelete(id);
+  
+      res.json(book);
+    } catch (error) {
+      res.sendStatus(500);
+  
+      console.log(error);
+    }
+  });
+  
+  module.exports = router;
+  

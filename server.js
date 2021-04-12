ac21f444f1107ccccb5c8171ac9110dce1d0c800
Caroline Lynch <carolineablynch@gmail.com>
2021-04-12 16:31:56 -0400

    connecting to mongoose

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const socketIO = require("socket.io");
 const routes = require("./routes");
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});

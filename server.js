df956d47d5dda805f952e4051a8bb68ea5e38188
Caroline Lynch <carolineablynch@gmail.com>
2021-04-12 16:32:36 -0400

    express

--- server.js
+++ server.js
@@ -14,3 +14,23 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useCreateIndex: true,
   useFindAndModify: false
 });
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") app.use(cors());
+
+app.use(express.static("build", { extensions: ["html"] }));
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(routes);
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+app.set("socketio", io);
+
+server.listen(PORT, () => console.log(`🚀 Listening on PORT ${PORT}`));

453a009c8b97941efea7cc463919100cb345a1f3
Caroline Lynch <carolineablynch@gmail.com>
2021-04-12 16:23:28 -0400

    building layout

---
+++ src/Layout.js
@@ -0,0 +1,6 @@
+import { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import socketIOClient from "socket.io-client";
+import Header from "./Header";
+import Footer from "./Footer";
